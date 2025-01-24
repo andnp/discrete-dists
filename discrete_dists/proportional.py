50fadc022e79b15802d835bc13cd36626bb9448f
Andy Patterson <andnpatterson@gmail.com>
2025-01-23 20:18:07 -0700

    feat: allow proportional dist to have shifted support

--- discrete_dists/proportional.py
+++ discrete_dists/proportional.py
@@ -1,17 +1,22 @@
 import numpy as np
-from discrete_dists.distribution import Distribution
+from discrete_dists.distribution import Distribution, Support
 from discrete_dists.utils.SumTree import SumTree
 
 class Proportional(Distribution):
-    def __init__(self, support: int):
+    def __init__(self, support: Support | int):
+        if isinstance(support, int):
+            support = (0, support)
+
         self._support = support
-        self.tree = SumTree(support)
+        rang = support[1] - support[0]
+        self.tree = SumTree(rang)
 
     # ---------------
     # -- Accessing --
     # ---------------
     def probs(self, idxs: np.ndarray) -> np.ndarray:
         idxs = np.asarray(idxs)
+        idxs = idxs - self._support[0]
 
         t = self.tree.total()
         if t == 0:
@@ -25,16 +30,18 @@ class Proportional(Distribution):
     # -- Sampling --
     # --------------
     def sample(self, rng: np.random.Generator, n: int) -> np.ndarray:
-        return self.tree.sample(rng, n)
+        return self.tree.sample(rng, n) + self._support[0]
 
     def stratified_sample(self, rng: np.random.Generator, n: int) -> np.ndarray:
-        return self.tree.stratified_sample(rng, n)
+        return self.tree.stratified_sample(rng, n) + self._support[0]
 
     # --------------
     # -- Updating --
     # --------------
     def update(self, idxs: np.ndarray, values: np.ndarray):
+        idxs = idxs - self._support[0]
         self.tree.update(idxs, values)
 
     def update_single(self, idx: int, value: float):
+        idx = idx - self._support[0]
         self.tree.update_single(idx, value)
